d9aa9732b3713f1079882633c0d3357a2b4ca370
Chris Allison <chris@quimarche.co.uk>
2013-11-07 08:54:13 +0000

    Fix analyzer warning: Cast of type 'SEL' (aka 'SEL *') to 'char *' is deprecated; use sel_getName instead.

--- Vendor/ActionSheetPicker/ActionSheetDatePicker.m
+++ Vendor/ActionSheetPicker/ActionSheetDatePicker.m
@@ -98,7 +98,7 @@
         return;
     }
     
-    NSAssert(NO, @"Invalid target/action ( %s / %s ) combination used for ActionSheetPicker", object_getClassName(target), (char *)action);
+    NSAssert(NO, @"Invalid target/action ( %s / %s ) combination used for ActionSheetPicker", object_getClassName(target), sel_getName(action));
 }
 
 - (void)eventForDatePicker:(id)sender {
